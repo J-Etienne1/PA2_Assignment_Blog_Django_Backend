c24de06161562292a879a61a2cca43335f7ab137
Jason <jasonetienne1@gmail.com>
2021-04-10 16:01:29 +0100

    blog.views line 19 and blog.urls causing issues when rying to make url for each post, commented out to see if  can get away without using

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,10 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.bloghomepage, name='bloghomepage'),
 
+    #causing issues
+    
+    #path('<title:post>/', views.post_single, name='post_single'),
+
 ]
+
+ #Test Blog Post
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import User
+from .models import Category
+from .models import Post
 
 # Create your views here.
 
 def bloghomepage(request):
-    return render(request, 'index.html')
+    all_Users = User.objects.all()
+    all_Category = Category.objects.all()
+    all_Post = Post.objects.all()
+
+
+    return render(request, 'index.html', {'post': all_Post, 'users': all_Users, 'category': all_Category})
+
+
+#causing issues
+
+#def post_single(request, post):
+#    post = get_object_or_404(Post, Title=post)
+#    return render(request, 'single.html', {'post': post})
